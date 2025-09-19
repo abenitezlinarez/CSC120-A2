2da685055b3212086bdb302c77075e63e1a0c7d7
Aris <abenitezlinarez@smith.edu>
2025-09-18 20:26:49 -0400

    I've edited my assignment

--- computer.py
+++ computer.py
@@ -1,10 +1,34 @@
 class Computer:
+# lines 4-10 list all the attributes the computer class stores
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
+    computer_id: int
+ # Here we create the constructor for the computer object attributes and methods
+    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int,
+                    operating_system: str, year_made: int, price: int, computer_id: int): 
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+        self.computer_id = computer_id
+    def printComputer(self):
+        print(self.description)
+#main allows us to run and test code, commented parts are where I've tested code    
+    
+    # First, let's make a computer
+    # Add it to the resale store's inventory
+ 
 
-    # What attributes will it need?
+    # Make sure it worked by checking inventory
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+   
 
-    # What methods will you need?
+# Calls the main() function when this file is run
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,88 @@
-class ResaleShop:
+from Computer import Computer
+from typing import Optional
+class resale_shop:
+    inventory : list = []
+    def __init__(self):
+        self.inventory = []
+        
+    def buy(self, Computer: Computer):
+        self.inventory.append(Computer)
+        return self.inventory.index(Computer)
 
-    # What attributes will it need?
+    def update_price(self, item_id: int, new_price: int):
+        if self.inventory[item_id] is not None:
+            self.inventory[item_id]["price"] = new_price
+        else:
+            print("Item", item_id, "not found. Cannot update price.")
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def sell(self,item_id: int):
+        if item_id < len(self.inventory) :
+            self.inventory.pop(item_id)
+            print("Item", item_id, "sold!")
+        else: 
+            print("Item", item_id, "not found. Please select another item to sell.")
 
-    # What methods will you need?
+    def print_inventory(self):
+        # If the inventory is not empty
+        if self.inventory:
+            # For each item
+            for item in self.inventory:
+                # Print its details
+                print(f'Item ID: {self.inventory.index(item)}')
+                item.printComputer()
+        else:
+            print("No inventory to display.")
+
+    def refurbish(self, item_id: int, new_os: Optional[str] = None):
+        if self.inventory[item_id] is not None:
+            computer = self.inventory[item_id] # locate the computer
+            if int(computer.year_made) < 2000:
+                computer.price = 0 # too old to sell, donation only
+            elif int(computer.year_made) < 2012:
+                computer.price = 250 # heavily-discounted price on machines 10+ years old
+            elif int(computer.year_made) < 2018:
+                computer.price = 550 # discounted price on machines 4-to-10 year old machines
+            else:
+                computer.price = 1000 # recent stuff
+
+            if new_os is not None:
+                computer.operating_system = new_os # update details after installing new OS
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
+
+def main():
+    computer1: Computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64,"macOS Big Sur", 2013, 1500, 1)
+    my_shop = resale_shop()
+    my_shop.print_inventory()
+       # Add it to the resale store's inventory
+    print("Buying", computer1.description)
+    print("Adding to inventory...")
+    computer_id = my_shop.buy(computer1)
+    print("Done.\n")
+    print("Checking inventory...")
+    my_shop.print_inventory()
+    print("Done.\n")
+     # Now, let's refurbish it
+    new_OS = "MacOS Monterey"
+    print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
+    print("Updating inventory...")
+    my_shop.refurbish(computer_id, new_OS)
+    print("Done.\n")
+
+    # Make sure it worked by checking inventory
+    print("Checking inventory...")
+    my_shop.print_inventory()
+    print("Done.\n")
+    
+    # Now, let's sell it!
+    print("Selling Item ID:", computer_id)
+    my_shop.sell(computer_id)
+    
+    # Make sure it worked by checking inventory
+    print("Checking inventory...")
+    my_shop.print_inventory()
+    print("Done.\n")
+
+    #my_shop.sell(computer_id)
+
+main()
